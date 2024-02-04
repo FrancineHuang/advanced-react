88262493960dd104f38bfb07e17bd38a265cf961
FrancineHuang <huangxin130@gmail.com>
2024-02-04 10:33:25 +0900

    add: error example

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import Starter from './tutorial/07-useRef/starter/01-useRef-basics'
-import Final from "./tutorial/01-useState/final/03-useState-array"
+
 function App() {
   return (
     <div className='container'>
       <Starter/>
-      <Final/>
     </div>
   );
 }
