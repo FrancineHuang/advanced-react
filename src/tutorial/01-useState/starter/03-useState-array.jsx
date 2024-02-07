8f655567387504bc43c34763149c428a8d9a223f
FrancineHuang <huangxin130@gmail.com>
2024-02-08 06:21:54 +0900

    add: use state array example

--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -4,14 +4,31 @@ import { data } from '../../../data'
 
 const UseStateArray = () => {
   const [people, setPeople] = useState(data)
-  return <div>
-    {people.map((person) => {
-      const{id, name} = person
-      return<dev key={id}>
-        {name}
-      </dev>
-    })}
-  </div>
-};
 
-export default UseStateArray;
+  const removeItem = (id) => {
+    const newPeople = people.filter((person => person.id !== id))
+    setPeople(newPeople)
+  }
+  const clearAllItems = () => {
+    setPeople([])
+  }
+
+  return (
+    <div>
+      {people.map((person) => {
+        const{id, name} = person
+        return(
+          <div key={id}>
+            <h4>{name}</h4>
+            <button type='button' onClick={() => removeItem(id)}>remove</button>
+          </div>
+        )
+      })}
+      <button type='button' style={{marginTop: '2rem'}} className='btn'>
+        Clear Items
+      </button>
+    </div>
+  )
+}
+
+export default UseStateArray
