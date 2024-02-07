cae29de6f1b50d176060250cf7d9e9b9e1f2505a
FrancineHuang <huangxin130@gmail.com>
2024-02-07 18:27:41 +0900

    add: useState setup

--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,17 @@
+import { React, useState } from 'react';
+
+import { data } from '../../../data'
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data)
+  return <div>
+    {people.map((person) => {
+      const{id, name} = person
+      return<dev key={id}>
+        {name}
+      </dev>
+    })}
+  </div>
 };
 
 export default UseStateArray;
