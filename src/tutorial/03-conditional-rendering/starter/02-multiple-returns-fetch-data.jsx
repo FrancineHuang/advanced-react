2bd41dfd112f71f620f643a0efef7bafcdfde2bc
FrancineHuang <huangxin130@gmail.com>
2024-02-16 06:59:02 +0900

    add: learned about the dependency rule of useEffect

--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -5,26 +5,26 @@ const MultipleReturnsFetchData = () => {
   const[isLoading, setIsLoading] = useState(true)
   const[isError, setIsError] = useState(false)
   const [user, setUser] = useState(null)
+  const fetchUser = async () => {
+    try {
+      const response = await fetch(url)
+
+      if(!response.ok) {
+        setIsError(true);
+        setIsLoading(false);
+      }
+
+      const user = await response.json()
+      //console.log(user)
+      setUser(user)
+    } catch (error) {
+      setIsError(true)
+      //console.log(error)
+    }
+    setIsLoading(false)
+  }
 
   useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const response = await fetch(url)
-
-        if(!response.ok) {
-          setIsError(true);
-          setIsLoading(false);
-        }
-
-        const user = await response.json()
-        //console.log(user)
-        setUser(user)
-      } catch (error) {
-        setIsError(true)
-        //console.log(error)
-      }
-      setIsLoading(false)
-    }
     fetchUser()
   }, [])
 
