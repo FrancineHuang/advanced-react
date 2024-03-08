71893de7102230128647c370f0c444550b62fb6b
FrancineHuang <huangxin130@gmail.com>
2024-03-09 00:04:56 +0900

    add: useRef basics

--- src/tutorial/07-useRef/starter/01-useRef-basics.jsx
+++ src/tutorial/07-useRef/starter/01-useRef-basics.jsx
@@ -2,10 +2,26 @@ import { useEffect, useRef, useState } from 'react';
 
 const UseRefBasics = () => {
   const [value, setValue] = useState(0);
+  const refContainer = useRef(null)
+  const isMounted = useRef(false)
 
   const handleSubmit = (e) => {
     e.preventDefault();
-  };
+    console.log(refContainer.current)
+    const name = refContainer.current.value
+    console.log(name)
+  }
+  useEffect(() => {
+    refContainer.current.focus()
+  })
+
+  useEffect(() => {
+    if(!isMounted.current) {
+      isMounted.current = true
+      return
+    }
+    console.log('re-render')
+  }, [value])
 
   return (
     <div>
@@ -14,7 +30,12 @@ const UseRefBasics = () => {
           <label htmlFor='name' className='form-label'>
             Name
           </label>
-          <input type='text' id='name' className='form-input' />
+          <input
+            type='text'
+            id='name'
+            className='form-input'
+            ref={refContainer}
+          />
         </div>
         <button type='submit' className='btn btn-block'>
           submit
