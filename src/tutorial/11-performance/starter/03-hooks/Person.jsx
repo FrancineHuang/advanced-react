256c40ca099e5ed5e781841d6ac318e41f6474b2
FrancineHuang <huangxin130@gmail.com>
2024-03-17 17:07:27 +0900

    add: useCallback hook

--- src/tutorial/11-performance/starter/03-hooks/Person.jsx
+++ src/tutorial/11-performance/starter/03-hooks/Person.jsx
@@ -1,7 +1,9 @@
-const Person = ({ name }) => {
+const Person = ({ name, id, removePerson }) => {
+  const onRemovePerson = () => removePerson(id)
   return (
     <div>
       <h4>{name}</h4>
+      <button onClick={onRemovePerson}>remove</button>
     </div>
   );
 };
